8d69a7cacbbad042e83b1b354db00d47b16abfe2
Zarif Atique <zarifatique02@gmail.com>
2025-06-08 14:03:04 +1000

    Implement edge class for undirected graph data structure

--- Graph(Edge list based)/edge.py
+++ Graph(Edge list based)/edge.py
@@ -7,4 +7,18 @@ class Edge(Generic[T]):
     _u: 'Vertex[T]'
     _v: 'Vertex[T]'
 
+    def __init__(self, element: T, u: 'Vertex[T]' = None, v: 'Vertex[T]' = None) -> None:
+        self._element = element 
+        self._u = u 
+        self._v = v
+
+    def get_element(self) -> T:
+        return self._element 
+    
+    def get_vertex_u(self) -> 'Vertex[T]':
+        return self._u 
+    
+    def get_vertex_v(self) -> 'Vertex[T]':
+        return self._v 
+    
     
